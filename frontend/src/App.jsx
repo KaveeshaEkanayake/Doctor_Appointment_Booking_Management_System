0fd1ecbc8b313c7f24940bd1b4fd70839d08ec95
SayuniAby <sayuniaby@gmail.com>
2026-03-12 16:42:51 +0530

    Create Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppointmentPage from "./pages/AppointmentPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  // return <AppointmentPage />;
+  return <LoginPage />;
+
+}
+
+export default App;
