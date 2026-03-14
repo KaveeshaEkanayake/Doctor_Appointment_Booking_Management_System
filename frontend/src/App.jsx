ff49021f0560073895441b70118b523488c26ad9
DinithiND <dinithidharmasena711@gmail.com>
2026-03-14 12:34:38 +0530

    created doctor login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DoctorRegistration from "./pages/DoctorRegistration";
+import DoctorLogin from "./pages/DoctorLogin";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/doctor-registration" element={<DoctorRegistration />} />
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/doctor-login" element={<DoctorLogin />} />
     </Routes>
   );
 }
