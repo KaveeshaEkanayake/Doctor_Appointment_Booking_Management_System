bc31ca6f74f0d7ab2ee571bd737ba5e5e38aedc8
DinithiND <dinithidharmasena711@gmail.com>
2026-03-18 02:23:04 +0530

    Add mobile responsive appointments page with book button and changed the alignements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DoctorRegistration from "./pages/DoctorRegistration";
-<<<<<<< HEAD
-import DoctorLogin from "./pages/DoctorLogin";
-import MyAppointments from "./pages/MyAppointments";
-=======
 import DoctorDashboard from "./pages/DoctorDashboard";
 import DoctorProfilePage from "./pages/DoctorProfilePage";
 import DoctorAvailabilityPage from "./pages/DoctorAvailabilityPage";
 import ProtectedRoute from "./components/ProtectedRoute";
->>>>>>> 7900a3e517b72aff568286b90ea06fa1ddb82ff4
+import MyAppointments from "./pages/MyAppointments";
 
 function App() {
   return (
@@ -20,14 +16,8 @@ function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-<<<<<<< HEAD
-      <Route path="/doctor-registration" element={<DoctorRegistration />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/doctor-login" element={<DoctorLogin />} />
-      <Route path="/my-appointments" element={<MyAppointments />} />
-
-=======
       <Route path="/doctor/register" element={<DoctorRegistration />} />
+      <Route path="/my-appointments" element={<MyAppointments />} />
 
       {/* Doctor protected routes */}
       <Route
@@ -54,7 +44,6 @@ function App() {
           </ProtectedRoute>
         }
       />
->>>>>>> 7900a3e517b72aff568286b90ea06fa1ddb82ff4
     </Routes>
   );
 }
