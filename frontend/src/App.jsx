0fa5837661e753573810247314c75bf587a21ace
Sithumi813 <sithumisanjana813@gmail.com>
2026-03-18 06:24:00 +0530

    Created Search & Filter Doctors Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import DoctorProfilePage from "./pages/DoctorProfilePage";
 import DoctorAvailabilityPage from "./pages/DoctorAvailabilityPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MyAppointments from "./pages/MyAppointments";
+import SearchFilterDoctors from "./pages/SearchFilterDoctors";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/doctor/register" element={<DoctorRegistration />} />
       <Route path="/my-appointments" element={<MyAppointments />} />
+      <Route path="/search-doctors" element={<SearchFilterDoctors />} />        
 
       {/* Doctor protected routes */}
       <Route
@@ -43,7 +45,7 @@ function App() {
             <DoctorAvailabilityPage />
           </ProtectedRoute>
         }
-      />
+      />      
     </Routes>
   );
 }
