a280cb28db45527c75fcb7db2d277292a8ca0373
Sithumi813 <sithumisanjana813@gmail.com>
2026-03-20 23:39:52 +0530

    Create Incoming Appointments for Doctor Page

---
+++ frontend/src/assets/appointmentsData.js
@@ -0,0 +1,69 @@
+export const initialPendingAppointments = [
+  {
+    id: 1,
+    name: "Sara Perera",
+    date: "Oct 24, 2023",
+    time: "09:30 AM",
+    reason: "Annual General Checkup",
+    image: "https://l-ldesign.com.au/2016/wp-content/uploads/2020/01/profile-pic-katie-square.jpg",
+  },
+  {
+    id: 2,
+    name: "Michael",
+    date: "Oct 24, 2023",
+    time: "11:15 AM",
+    reason: "Follow-up on Blood Tests",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ntUU7AzmOxa5HB8zS83sa-JFHEfZJAoI2A&s",
+  },
+  {
+    id: 3,
+    name: "Amanda",
+    date: "Oct 25, 2023",
+    time: "02:00 PM",
+    reason: "Seasonal Allergy Consultation",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQugb_Bwq4G4QY6V11WR8dMyDTJnX6JbleZ1A&s",
+  },
+  {
+    id: 4,
+    name: "David Silva",
+    date: "Oct 26, 2023",
+    time: "10:45 AM",
+    reason: "Post-surgery Review",
+    image: "https://i0.wp.com/www.commercialphotographynorthwestblog.co.uk/wp-content/uploads/2020/10/MDH_8729webqualitysquare.jpg?resize=560%2C560&ssl=1",
+  },
+];
+
+export const initialConfirmedAppointments = [
+  {
+    id: 10,
+    name: "Sarah Perera",
+    date: "Today, Oct 24",
+    time: "10:00 AM",
+    reason: "Post-surgery follow-up (Heart Valve)",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsTXTE3FBqvPGLwVwasuF4iHYpSa01U9YkfA&s",
+  },
+  {
+    id: 11,
+    name: "Robert P",
+    date: "Today, Oct 24",
+    time: "11:30 AM",
+    reason: "Hypertension management checkup",
+    image: "https://sholaemmanuel.com/wp-content/uploads/2024/06/photo-1535713875002-d1d0cf377fde.jpg",
+  },
+  {
+    id: 12,
+    name: "Emily Silva",
+    date: "Tomorrow, Oct 25",
+    time: "09:15 AM",
+    reason: "Routine echo-cardiogram",
+    image: "https://miro.medium.com/v2/1*6KtkC2T-cG4uV-dWn0NQlA.jpeg",
+  },
+  {
+    id: 13,
+    name: "Michelle Silva",
+    date: "Tomorrow, Oct 25",
+    time: "02:45 PM",
+    reason: "Lab results discussion",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8d4F3Lf3kbFIFSGu6BSqqThC9vsueKd7a_w&s",
+  },
+];
