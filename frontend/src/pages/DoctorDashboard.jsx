a8136af897b34378b604c12551fb8c31354b6d93
DinithiND <dinithidharmasena711@gmail.com>
2026-04-08 13:32:10 +0530

    created doctor dashboard

--- frontend/src/pages/DoctorDashboard.jsx
+++ frontend/src/pages/DoctorDashboard.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import DoctorLayout from "../layouts/DoctorLayout";
-
-// export default function DoctorDashboard() {
-//   return (
-//     <DoctorLayout>
-//       <div className="p-8">
-//         <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-//         <p className="text-gray-500 mt-2">
-//           Welcome to your doctor dashboard. This page is under construction.
-//         </p>
-//       </div>
-//     </DoctorLayout>
-//   );
-// }
-
-
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DoctorLayout from "../layouts/DoctorLayout";
